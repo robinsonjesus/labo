cb39ec783931eabd7f8826e4badfa3bdbfbbebbb
tomasgarciafo <103579086+tomasgarciafo@users.noreply.github.com>
2022-06-02 19:05:29 -0300

    543 lightGBM BO nube

--- src/xgboost/562_xgboost_original_default.R
+++ src/xgboost/562_xgboost_original_default.R
@@ -72,10 +72,10 @@ EstimarGanancia_xgboost  <- function( x )
                           alpha=                0.0,  #por ahora, lo dejo fijo, equivalente a  lambda_l1
                           lambda=               0.0,  #por ahora, lo dejo fijo, equivalente a  lambda_l2
                           subsample=            1.0,  #por ahora, lo dejo fijo
-                          tree_method=       "hist",  # histograma
-                          grow_policy=  "lossguide",  # lossguide
-                          max_depth=            0,    # NO lo limito por altura
+                          tree_method=       "auto",  #por ahora lo dejo fijo, pero ya lo voy a cambiar a "hist"
+                          grow_policy=  "depthwise",  #ya lo voy a cambiar a "lossguide"
                           max_bin=            256,    #por ahora fijo
+                          max_leaves=           0,    #ya lo voy a cambiar
                           scale_pos_weight=     1.0   #por ahora, lo dejo fijo
                         )
 
@@ -130,12 +130,12 @@ dataset  <- fread("./datasets/paquete_premium_202011.csv")
 #creo la carpeta donde va el experimento
 # HT  representa  Hiperparameter Tuning
 dir.create( "./labo/exp/",  showWarnings = FALSE ) 
-dir.create( "./labo/exp/HT5720/", showWarnings = FALSE )
-# setwd("D:\\gdrive\\ITBA2022A\\labo\\exp\\HT5720\\")   #Establezco el Working Directory DEL EXPERIMENTO
-setwd("C:\\Users\\Tomás García\\Documents\\MD\\labo\\exp\\HT5720\\")   #Establezco el Working Directory DEL EXPERIMENTO
+dir.create( "./labo/exp/HT5620/", showWarnings = FALSE )
+setwd("D:\\gdrive\\ITBA2022A\\labo\\exp\\HT5620\\")   #Establezco el Working Directory DEL EXPERIMENTO
+
 
 #en estos archivos quedan los resultados
-klog        <- "HT572.txt"
+klog        <- "HT562.txt"
 
 
 GLOBAL_iteracion  <- 0   #inicializo la variable global
@@ -165,7 +165,7 @@ dtrain  <- xgb.DMatrix( data=  data.matrix(  dataset[ , campos_buenos, with=FALS
 x  <- list( eta=               0.3,
             colsample_bytree=  1.0,
             min_child_weight=  1.0,
-            max_leaves=       20,
+            max_depth=         6,
             prob_corte=        1/60
           )
 
